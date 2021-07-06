5a81edd409d2dfc38c125a79a9a3bb56ce75dd10
RAKOTOMAMONJY Sitraka <mbolasitrakaa@gmail.com>
2021-07-06 21:02:08 +0200

    add page login, header, footer,master

---
+++ resources/views/login.blade.php
@@ -0,0 +1,20 @@
+@extends('master')
+@section("content")
+<div class="container custom-login">
+    <div class="row">
+        <div class="col-sm-4 col-sm-offset-4">
+            <form>
+                <div class="form-group">
+                    <label for="inputMail">Email address</label>
+                    <input type="email" class="form-control" id="inputMail" placeholder="mail">
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword">Password</label>
+                    <input type="password" class="form-control" id="inputPassword" placeholder="password">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
