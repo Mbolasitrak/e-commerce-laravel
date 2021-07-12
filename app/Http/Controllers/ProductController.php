650e3b7912c1604bb207abfa669665fa588683e0
RAKOTOMAMONJY Sitraka <mbolasitrakaa@gmail.com>
2021-07-12 23:14:41 +0200

    modifie fichier et creation Login et logout

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Session;
 
 class ProductController extends Controller
 {
@@ -20,6 +22,23 @@ class ProductController extends Controller
         //return $req->input();
         $data=Product::where('name','like','%'.$req->input('query').'%')->get();
         return view('search',['products'=>$data]);
+    }
+    function addToCart(Request $req){
+        if($req->session()->has('user')){
+            $cart = new Cart;
+            $cart->user_id = $req->session()->get('user')['id'];
+            $cart->product_id=$req->product_id;
+            $cart->save();
+            return redirect('/');
+        }
+        else{
+            return redirect('/login');
+        }
+
+    }
+    static function cartItem(){
+        $userId=Session::get('user')['id'];
+        return Cart::WHERE('user_id',$userId)->count();
     }
 
 }
